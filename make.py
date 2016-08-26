f14ee1479981d8435b0db8630eaeb46cc9938161
kablaa <tlukasiewicz@knights.ucf.edu>
2016-08-25 21:43:19 -0400

    Simplified

--- make.py
+++ make.py
@@ -2,26 +2,15 @@ import time
 import sys
 from subprocess import call
 
-def usage():
-    print "usage: " + sys.argv[0] + " <file to watch> <make command (optional)>"
-
 def getData(fileName):
     f = open(fileName,"r")
     data = f.read()
     f.close()
     return data
 
-numArgs = len(sys.argv)
 fileName = sys.argv[1]
 sleepTime = 2
-
-if numArgs < 2:
-    usage()
-    exit()
-
-command = "make"
-if numArgs > 2:
-    command = [command,sys.argv[2]]
+command = ["make",sys.argv[2]]
 
 data = getData(fileName)
 while True:
